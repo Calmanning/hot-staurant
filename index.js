f126924f952721b5e75c8e1a590b3560c0a27aad
That Guy Cal <calmanning@gmail.com>
2021-01-16 16:52:15 -0800

    data routes complete.

--- index.js
+++ index.js
@@ -14,10 +14,66 @@ app.use(express.static("public"));
 // app.use("/", htmlRoutes);
 // app.use("/api", apiRoutes);
 
+//data
+    const tables = [
+        {
+            name: "TheTabler",
+            id: 1,
+            email: "got@table.teabow",
+            phone: "1234567890",
+        }
+    ];
+
+    const waitlist = [
+        {
+            name: "noTable",
+            id: 2,
+            email: "sorry@table.no",
+            phone: "0987654321",
+        }
+    ];
+
+// HTML Routes
 app.get('/', (req,res) => {
     res.send("Welcome. Now we can get to the real work.");
 })
 
+app.get('/reserve', (req,res) => {
+    res.send("reserve page");
+})
+//END HTML Routes
+
+// API Routes
+app.get('/api/tables', (req,res) => {
+    res.json(tables);
+})
+
+app.get("/api/waitlist", (req,res) => {
+    res.json(waitlist);
+})
+
+app.post("/reserve", (req, res) => {
+    
+    console.log(req.body);
+    if (tables.length<5){
+        tables.push(req.body)
+        const responseObj = {
+            hasTable: true,
+            userData: req.body
+        }
+        res.json(responseObj);
+    } else {
+        waitlist.push(req.body)
+        const responseObj = {
+            hasTable: false,
+            userDate: req.body
+            }
+        res.json(responseObj);
+        }
+        
+})//END API Routes
+
+//server listener
 app.listen(PORT, function () {
     console.log("We're listening over at http://localhost:" + PORT);
 });
